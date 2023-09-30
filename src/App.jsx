ff83c2b0f80b08f2dd7831d8c8fa3012d8c768e9
frknecn3 <furkanduhannuman@gmail.com>
2023-09-30 13:51:03 +0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
 import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
-import "./index.css"
 
 
 const App=()=> {
